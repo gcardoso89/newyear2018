968b958c026fad61c15c509797ddcbda301b23e4
gcardoso89 <goncalo.cardoso@safetyculture.com>
2018-01-16 13:25:47 +1100

    Update texts before launching

--- finals.js
+++ finals.js
@@ -4,7 +4,7 @@ module.exports = {
 		facebook: "Sans surprise, je suis le client type angoissé du brief ! D'autres profiles à découvrir sur http://moiclient.fr, le test pour savoir quel client vous êtes ou seriez dans le monde impitoyable des agences de communication !",
 		title: "Angoissé du brief",
 		description: "La pression, vous connaissez. Perfectionniste, vous vous fixez des objectifs toujours très élevés. Votre hiérarchie l’a compris et sait en abuser. Ultra carré, vous ne laissez rien au hasard. Le brief, c’est un peu votre Taj Mahal, une œuvre d’art inachevée… Principale qualité : votre anticipation extrême permet souvent de gagner un temps précieux. Petit défaut : votre stress est parfois communicatif… apprenez à décompresser :-)",
-		tendency: "A ce rythme là, vous risquez de brûler la chandelle par les deux bouts. Ralentissez, soufflez et choisissez vos combats. Finalement, la pression, c’est avant tout vous qui vous la mettez, non ?",
+		tendency: "À ce rythme là, vous risquez de brûler la chandelle par les deux bouts. Ralentissez, soufflez et choisissez vos combats. Finalement, la pression, c’est avant tout vous qui vous la mettez, non ?",
 		advice: "Vous faites partie des clients les plus stimulants. D’ailleurs on aime vous rassurer et vous sentir plus détendu. Parfois, on se dit même que l’agence c’est un peu votre doudou. Alors serrez-nous très fort contre vous et appelez nous souvent… Vous verrez ça ira tout de suite beaucoup mieux."
 	},
 	"baroque-multicolore": {
@@ -19,10 +19,10 @@ module.exports = {
 		twitter: "Sans surprise, je suis le client type vintage ! D'autres profiles à découvrir sur http://moiclient.fr, le #test pour savoir quel client vous êtes ou seriez dans le monde impitoyable des agences de communication ! #4aout",
 		facebook: "Sans surprise, je suis le client type vintage ! D'autres profiles à découvrir sur http://moiclient.fr, le test pour savoir quel client vous êtes ou seriez dans le monde impitoyable des agences de communication !",
 		title: "Vintage",
-		description: "Dans votre sacoche, un Nokia 3310 qui « marche encore très bien » et dont vous ne vous sépareriez pour rien au monde. D’ailleurs, le SMS reste votre moyen de communication préféré. Disons-le tout de suite : côté nouvelles technologies, vous vous êtes arrêté à Club Internet, voire au « tatoo ». Mais vous restez à l’écoute et ne manquez pas d’idées. Il vous arrive même parfois de vous lancer dans des projets qui peuvent nous faire sourire… Pour vous, un site internet se conçoit forcément au « format A4 ». \n" +
-		"A l’inverse, vous êtes un interlocuteur de haut niveau sur tous les projets d’édition. Chaque choix typo doit être justifié, aucune césure ne vous échappe, vous repérez un quart de cadratin insécable en un dixième de seconde... Bref, vous êtes un artisan de la communication, un orfèvre du papier et des mots.",
+		description: "Dans votre sacoche, un Nokia 3310 qui « marche encore très bien » et dont vous ne vous sépareriez pour rien au monde. D’ailleurs, le SMS reste votre moyen de communication préféré. Disons-le tout de suite : côté nouvelles technologies, vous vous êtes arrêté à Club Internet, voire au « Tatoo ». Mais vous restez à l’écoute et ne manquez pas d’idées. Il vous arrive même parfois de vous lancer dans des projets qui peuvent nous faire sourire… Pour vous, un site internet se conçoit forcément au « format A4 ». \n" +
+		"À l’inverse, vous êtes un interlocuteur de haut niveau sur tous les projets d’édition. Chaque choix typo doit être justifié, aucune césure ne vous échappe, vous repérez un quart de cadratin insécable en un dixième de seconde... Bref, vous êtes un artisan de la communication, un orfèvre du papier et des mots.",
 		tendency: "Le papier a encore de belles années devant lui, rassurez-vous. Faites-vous plaisir avec de beaux supports d’édition : catalogues, rapports d’activité, brochures… Mais tentez de penser à leur pendant digital. C’est juste une question d’entraînement, vous verrez.",
-		advice: "Chez 4août, on adore l’édition. Rien ne nous fait vibrer autant qu’un grammage voluptueux ou une dorure à chaud. Pourtant, on pense aussi intelligence artificielle et cross média. Et si on progressait ensemble en 2018 ?"
+		advice: "Chez 4août, on adore l’édition. Rien ne nous fait vibrer autant qu’un grammage voluptueux ou une dorure à chaud. Pourtant, on pense aussi intelligence artificielle et cross-média. Et si on progressait ensemble en 2018 ?"
 	},
 	"cost-killer": {
 		twitter: "Sans surprise, je suis un cost-killer ! D'autres profiles à découvrir sur moiclient.fr, le #test pour savoir quel client vous êtes ou seriez dans le monde impitoyable des agences de communication ! #4aout",
@@ -30,7 +30,7 @@ module.exports = {
 		title: "Cost-killer",
 		description: "Les chiffres, c’est votre dada. Excellent gestionnaire, vous veillez à votre budget comme le lait sur le feu. Vous êtes un addict du black Friday et ne ratez jamais une vente privée. Exigeant, vous pouvez être parfois dur en affaires, avec une légère tendance à vouloir casser les prix. Pour les plus rigoureux d’entre vous, la présence en réunion se chiffre aisément à la minute…",
 		tendency: "Restez comme vous êtes ! Votre ténacité sera saluée. Maintenez le cap de la rigueur tout en gardant l’esprit ouvert… Mettez vos bonnes résolutions en pratique : prenez du recul et autorisez-vous un break décadent de quelques jours. Vous l’avez bien mérité !",
-		advice: "Continuez à nous challenger :-) Et surtout : prenez du temps pour vous, ça nous ferait plaisir ! A ce sujet : et si on déjeunait ensemble en janvier ?"
+		advice: "Continuez à nous challenger :-) Et surtout : prenez du temps pour vous, ça nous ferait plaisir ! À ce sujet : et si on déjeunait ensemble en janvier ?"
 	},
 	"bff-best-friend-forever": {
 		twitter: "Sans surprise, je suis le client type Best Friend Forever ! D'autres profiles à découvrir sur http://moiclient.fr, le test pour savoir quel client vous êtes ou seriez dans le monde impitoyable des agences de communication ! #4aout",
